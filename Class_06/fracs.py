a6147858edb496843f317f3bc3cfed92ea869c08
nadlej <piotr.przemielewski@gmail.com>
2020-11-25 10:43:09 +0100

    Update fracs.py

--- Class_06/fracs.py
+++ Class_06/fracs.py
@@ -1,8 +1,9 @@
 from math import gcd
 import unittest
 
-#W pliku fracs.py zdefiniować klasę Frac wraz z potrzebnymi metodami.
-#Ułamek jest reprezentowany przez parę liczb całkowitych. Napisać kod testujący moduł fracs.
+
+# W pliku fracs.py zdefiniować klasę Frac wraz z potrzebnymi metodami.
+# Ułamek jest reprezentowany przez parę liczb całkowitych. Napisać kod testujący moduł fracs.
 
 
 class Frac:
@@ -12,46 +13,44 @@ class Frac:
         self.x = x
         self.y = y
 
-    def __str__(self):         # zwraca "x/y" lub "x" dla y=1
+    def __str__(self):  # zwraca "x/y" lub "x" dla y=1
         if self.y == 1:
             return "{}".format(self.x)
         else:
             return "{0} / {1}".format(self.x, self.y)
 
-    def __repr__(self):        # zwraca "Frac(x, y)"
-        return 'Frac({},{})'.format(self.x,self.y)
+    def __repr__(self):  # zwraca "Frac(x, y)"
+        return 'Frac({},{})'.format(self.x, self.y)
 
     def __eq__(self, other):
-        return 1 if self.__sub__(other)[0] == 0 else 0
+        return 1 if self.x * other.y == other.x * self.y else 0
 
     def __ne__(self, other):
-        return 1 if self.__sub__(other)[0] != 0 else 0
+        return 1 if self.x * other.y != other.x * self.y else 0
 
     def __lt__(self, other):
-        nb = self.__sub__(other)
-        return 1 if nb[0] < 0 else 0
+        return 1 if self.x * other.y < other.x * self.y else 0
 
     def __le__(self, other):
-        nb = self.__sub__(other)
-        return 1 if nb[0] > 0 else 0
+        return 1 if self.x * other.y > other.x * self.y else 0
 
-    def __add__(self, other): # frac1 + frac2
+    def __add__(self, other):  # frac1 + frac2
         nb = [self.x * other.y + other.x * self.y, self.y * other.y]
         mod = gcd(nb[0], nb[1])
 
-        return [nb[0] / mod, nb[1] / mod]
+        return Frac(nb[0] / mod, nb[1] / mod)
 
     def __sub__(self, other):  # frac1 - frac2
         nb = [self.x * other.y - other.x * self.y, self.y * other.y]
         mod = gcd(nb[0], nb[1])
 
-        return [nb[0] / mod, nb[1] / mod]
+        return Frac(nb[0] / mod, nb[1] / mod)
 
     def __mul__(self, other):  # frac1 * frac2
         nb = [self.x * other.x, self.y * other.y]
         mod = gcd(nb[0], nb[1])
 
-        return [nb[0] / mod, nb[1] / mod]
+        return Frac(nb[0] / mod, nb[1] / mod)
 
     def __div__(self, other):  # frac1 / frac2
         if other.x == 0:
@@ -59,7 +58,13 @@ class Frac:
         nb = [self.x * other.y, self.y * other.x]
         mod = gcd(nb[0], nb[1])
 
-        return [nb[0] / mod, nb[1] / mod]
+        return Frac(nb[0] / mod, nb[1] / mod)
+
+    def __truediv__(self, other):  # frac1 / frac2, Python 3
+        return float(self) / float(other)
+
+    def __hash__(self):
+        return hash(float(self))  # immutable fracs
 
     # operatory jednoargumentowe
     def __pos__(self):  # +frac = (+1)*frac
@@ -71,8 +76,9 @@ class Frac:
     def __invert__(self):  # odwrotnosc: ~frac
         return Frac(self.y, self.x)
 
-    def __float__(self):       # float(frac)
+    def __float__(self):  # float(frac)
         return self.x / self.y
+
 
 # Kod testujący moduł.
 
@@ -80,9 +86,9 @@ class Frac:
 class TestFrac(unittest.TestCase):
     def setUp(self):
         self.f0 = Frac(0, 1)
-        self.f1 = Frac(3,6)
-        self.f2 = Frac(-1,3)
-        self.f3 = Frac(7,10)
+        self.f1 = Frac(3, 6)
+        self.f2 = Frac(-1, 3)
+        self.f3 = Frac(7, 10)
 
     def test_str(self):
         self.assertEqual(self.f3.__str__(), "7 / 10")
@@ -109,27 +115,31 @@ class TestFrac(unittest.TestCase):
         self.assertTrue(self.f1.__le__(self.f0))
 
     def test_add(self):
-        self.assertEqual(self.f1.__add__(self.f0), [1,2])
-        self.assertEqual(self.f2.__add__(self.f3), [11,30])
-        self.assertEqual(self.f1.__add__(self.f2), [1,6])
+        self.assertEqual(self.f1.__add__(self.f0), Frac(1, 2))
+        self.assertEqual(self.f2.__add__(self.f3), Frac(11, 30))
+        self.assertEqual(self.f1.__add__(self.f2), Frac(1, 6))
 
     def test_sub(self):
-        self.assertEqual(self.f1.__sub__(self.f0), [1,2])
-        self.assertEqual(self.f2.__sub__(self.f3), [-31,30])
-        self.assertEqual(self.f1.__sub__(self.f2), [5,6])
+        self.assertEqual(self.f1.__sub__(self.f0), Frac(1, 2))
+        self.assertEqual(self.f2.__sub__(self.f3), Frac(-31, 30))
+        self.assertEqual(self.f1.__sub__(self.f2), Frac(5, 6))
 
     def test_mul(self):
-        self.assertEqual(self.f1.__mul__(self.f0), [0,1])
-        self.assertEqual(self.f2.__mul__(self.f3), [-7,30])
-        self.assertEqual(self.f1.__mul__(self.f2), [-1,6])
+        self.assertEqual(self.f1.__mul__(self.f0), Frac(0, 1))
+        self.assertEqual(self.f2.__mul__(self.f3), Frac(-7, 30))
+        self.assertEqual(self.f1.__mul__(self.f2), Frac(-1, 6))
 
     def test_div(self):
         self.assertRaises(ZeroDivisionError, self.f1.__div__, self.f0)
-        self.assertEqual(self.f2.__div__(self.f3), [-10,21])
-        self.assertEqual(self.f1.__div__(self.f2), [3,-2])
+        self.assertEqual(self.f2.__div__(self.f3), Frac(-10, 21))
+        self.assertEqual(self.f1.__div__(self.f2), Frac(3, -2))
 
     def test_frac2float(self):
         self.assertEqual(self.f2.__float__(), -1 / 3)
+
+    def test_truediv(self):
+        self.assertEqual(self.f2.__truediv__(self.f3), (-1 / 3) / (7 / 10))
+        self.assertEqual(self.f3.__truediv__(self.f1), (7 / 10) / (3 / 6))
 
     def tearDown(self): pass
 
