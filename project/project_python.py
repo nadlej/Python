1db814003744d5c9607615ad143e945f6b474b5d
nadlej <piotr.przemielewski@gmail.com>
2021-01-16 14:36:11 +0100

    Update project_python.py

--- project/project_python.py
+++ project/project_python.py
@@ -1,5 +1,6 @@
 import copy
 import unittest
+from math import ceil
 
 
 class Edge:
@@ -53,140 +54,104 @@ class Graph:
         self.n = n  # kompatybilność
         self.directed = directed  # bool, czy graf skierowany
         self.adj = [[] for i in range(self.n)]  # główna lista
-        self.is_vector = [0] * self.n  # tablica istnienia danych wierzchołków
-        self.e = 0  # liczba krawedzi
-        self.v = 0  # liczba wierzchołków
+        self.edges_count = 0  # liczba krawedzi
         self.getDFS = []  # lista zwracająca DFS dla danego wierzchołka
         self.getBFS = []  # lista zwracająca BFS dla danego wierzchołka
 
     def v(self):  # zwraca liczbę wierzchołków
-        return self.v
+        return self.n
 
     def e(self):  # zwraca liczbę krawędzi
-        return self.e
+        return self.edges_count
 
     def is_directed(self):  # bool, czy graf skierowany
         return self.directed
 
     def add_node(self, node):  # dodaje wierzchołek
-        assert self.n != self.v, "max number of vectors"
-
-        self.v += 1
-        self.is_vector[node] = 1
+        assert self.n >= node
 
     def has_node(self, node):  # bool
-        if self.is_vector[node] == 1:
-            return True
-        else:
-            return False
+        return node < self.n
 
     def del_node(self, node):  # usuwa wierzchołek
-        assert self.is_vector[node] == 1, "node is not exist"
-
-        edges_list = []
-        for k in self.adj:
-            if len(k) != 0:
-                edges_list.extend(k)
+        assert node < self.n, "node is not exist"
 
         if self.is_directed():
-            for edge in edges_list:
-                if edge.source == node:
-                    self.e -= len(self.adj[node])
-                    self.adj[node].clear()
-                elif edge.target == node:
-                    self.adj[edge.source].remove(edge)
-                    self.e -= 1
-        else:
-            for edge in edges_list:
-                if edge.source == node:
-                    self.e -= len(self.adj[node])
-                    self.adj[node].clear()
-                elif edge.target == node:
-                    self.adj[edge.source].remove(edge)
+            self.edges_count -= len(self.adj[node])
+            self.adj[node].clear()
 
-        self.v -= 1
-        self.is_vector[node] = 0
+            for vector in self.adj:
+                if node in vector:
+                    vector.remove(node)
+                    self.edges_count -= 1
+
+        else:
+            self.edges_count -= len(self.adj[node])
+            self.adj[node].clear()
+
+            for vector in self.adj:
+                if node in vector:
+                    vector.remove(node)
 
     def add_edge(self, edge):  # wstawienie krawędzi
         assert edge.source < self.n and edge.target < self.n, "vector out of max"
         if self.is_directed():  # dodajemy krawedz do listy danego wierzcholka
-            self.adj[edge.source].append(edge)
-            self.e += 1
+            self.adj[edge.source].append(edge.target)
+            self.edges_count += 1
         else:  # dodajemy krawedz do listy danego wierzcholka i do jego celu
             if edge.source != edge.target:
-                self.adj[edge.target].append(edge.__invert__())
-                self.adj[edge.source].append(edge)
+                self.adj[edge.target].append(edge.source)
+                self.adj[edge.source].append(edge.target)
             else:
-                self.adj[edge.source].append(edge)
-            self.e += 1
+                self.adj[edge.source].append(edge.target)
+            self.edges_count += 1
 
     def has_edge(self, edge):  # bool
-        if self.is_vector[edge.source] == 0:
-            return False
-        else:
-            is_true = False
-            for k in self.adj[edge.source]:
-                if k.__cmp__(edge) == 0:
-                    is_true = True
-            return is_true
+        return edge.target in self.adj[edge.source]
 
     def del_edge(self, edge):  # usunięcie krawędzi
         assert edge.source < self.n and edge.target < self.n, "vector out of max"
+        assert edge.target in self.adj[edge.source]
 
         if self.is_directed():  # iterujemy po wszystkich krawędziach wierzchołka źródłowego i porównujemy
-            for iter_ in self.adj[edge.source]:
-                if iter_.__cmp__(edge) == 0:
-                    self.adj[edge.source].remove(iter_)
-                    self.e -= 1
+            self.adj[edge.source].remove(edge.target)
         else:  # iterujemy po wszystkich krawędziach wierzchołka źródłowego i docelowego i porównujemy
-            for iter_ in self.adj[edge.source]:
-                if iter_.__cmp__(edge) == 0:
-                    self.adj[edge.source].remove(iter_)
-
-            for iter_ in self.adj[edge.target]:
-                if iter_.__cmp__(edge.__invert__()) == 0:
-                    self.adj[edge.target].remove(iter_)
-                    self.e -= 1
+            self.adj[edge.source].remove(edge.target)
+            self.adj[edge.target].remove(edge.source)
+        self.edges_count -= 1
 
     def weight(self, edge):  # zwraca wagę krawędzi
         return edge.weight
 
     def iternodes(self):  # iterator po wierzchołkach
-        nodes = []
-        for k in range(0, self.n):
-            if self.is_vector[k] == 1:
-                nodes.append(k)
-        return nodes
+        return [i for i in range(0, self.n)]
 
     def iteradjacent(self, node):  # iterator po wierzchołkach sąsiednich
-        nodes = [k.target for k in self.adj[node]]
-        return nodes
+        return self.adj[node]
 
     def iteroutedges(self, node):  # iterator po krawędziach wychodzących
         edges = ""
         for i in self.adj[node]:
-            edges += i.__repr__() + '\n'
+            new_edge = Edge(node, i)
+            edges += new_edge.__repr__() + '\n'
         return edges
 
     def iterinedges(self, node):  # iterator po krawędziach przychodzących
         edges = ""
-        edges_list = []
-        for k in self.adj:
-            if len(k) != 0:
-                edges_list.extend(k)
 
-        for k in edges_list:
-            if k.target == node:
-                edges += k.__repr__() + '\n'
+        for k in self.adj:
+            if node in k:
+                new_edge = Edge(self.adj.index(k), node)
+                edges += new_edge.__repr__() + '\n'
 
         return edges
 
     def iteredges(self):  # iterator po krawędziach
         edges = ""
-        for k in self.adj:
-            if len(k) != 0:
-                for l in k:
-                    edges += l.__repr__() + '\n'
+        for i in range(0, self.n):
+            for k in self.adj[i]:
+                new_edge = Edge(i, k)
+                edges += new_edge.__repr__() + '\n'
 
         return edges
 
@@ -200,13 +165,9 @@ class Graph:
             for k in new_graph.adj:
                 k.clear()
 
-            edges_list = []  # tworzymy listę wierzchołków "starego" grafu
-            for k in self.adj:
-                if len(k) != 0:
-                    edges_list.extend(k)
-
-            for k in edges_list:  # dodajemy "odwrócne" krawedzie do "nowego" grafu, wierzchołki się nie zmieniają
-                new_graph.add_edge(k.__invert__())
+            for i in range(0, self.n):  # dodajemy "odwrócne" krawedzie do "nowego" grafu, wierzchołki się nie zmieniają
+                for k in self.adj[i]:
+                    new_graph.add_edge(Edge(k, i))
 
             return new_graph
         else:  # jeżeli graf nie jest skierowany, graf transponowany zwróci to samo
@@ -214,65 +175,51 @@ class Graph:
 
     def complement(self):  # zwraca dopełnienie grafu
         new_graph = self.copy()
-        edges = []
-        nodes = self.iternodes()
-        subnodes = []
 
-        for k in self.adj:  # tworzymy listę wszystkich krawędzi
-            if len(k) != 0:
-                edges.extend(k)
-
-        for i in nodes:  # tworzymy listę aktywnych wierzchołków i czyścimy listy ich krawędzi w nowym grafie
+        for i in range(0, self.n):  # czyścimy listy krawędzi w nowym grafie
             new_graph.adj[i].clear()
 
         if self.is_directed() is True:  # tworzymy wszystkie nowe połaczenia, ktore nie pojawiły się wcześniej
-            for i in nodes:
-                for edge in self.adj[i]:
-                    subnodes.append(edge.target)
-                for j in nodes:
-                    if j not in subnodes:
-                        new_graph.add_edge(Edge(i, j))
-                subnodes.clear()
+            for i in range(0, self.n):
+                for j in range(0, self.n):
+                    if j not in self.adj[i]:
+                        new_edge = Edge(i, j)
+                        new_graph.add_edge(new_edge)
         else:
-            for i in nodes:
-                for edge in self.adj[i]:
-                    subnodes.append(edge.target)
-                for j in nodes:
-                    if j not in subnodes and new_graph.has_edge(
-                            Edge(i, j)) is False:  # "odwrócone" (1;1) to wciąż (1;1)
-                        new_graph.add_edge(Edge(i, j))
-                subnodes.clear()
+            for i in range(0, ceil(self.n / 2)):
+                for j in range(0, self.n):
+                    if j not in self.adj[i]:
+                        new_edge = Edge(i, j)
+                        new_graph.add_edge(new_edge)
 
         return new_graph
 
     def subgraph(self, nodes):  # zwraca podgraf indukowany
         new_graph = Graph(self.n, self.directed)
-
-        for i in nodes:  # oznaczamy przeslane wierzchołki jako aktywne
-            new_graph.is_vector[i] = 1
+        nodes_copy = nodes.copy()
 
         if self.is_directed() is True:
-            for k in self.adj:
-                if len(k) != 0:
-                    for l in k:  # sprawdzamy, czy wierzchołek źródłowy i jego cel znajdują się w liście
-                        if l.source in nodes and l.target in nodes:
-                            new_graph.add_edge(Edge(l.source, l.target, l.weight))
+            for i in nodes:
+                for target in self.adj[i]:  # sprawdzamy, czy wierzchołek źródłowy i jego cel znajdują się w liście
+                    if target in nodes:
+                        new_edge = Edge(i, target)
+                        new_graph.add_edge(new_edge)
         else:
-            for k in self.adj:
-                if len(k) != 0:
-                    for l in k:
-                        if l.source in nodes \
-                                and l.target in nodes and new_graph.has_edge(Edge(l.source, l.target,
-                                                                                  l.weight)) is False:
-                            new_graph.add_edge(Edge(l.source, l.target, l.weight))
+            for i in nodes:
+                if i in nodes_copy:
+                    for target in self.adj[i]:  # sprawdzamy, czy wierzchołek źródłowy i jego cel znajdują się w liście
+                        if target in nodes:
+                            new_edge = Edge(i, target)
+                            new_graph.add_edge(new_edge)
+                            nodes_copy.remove(target)
 
         return new_graph
 
-    def DFS(self, v):
-        assert v >= 0 and self.is_vector[v] == 1 and self.is_directed() is True
+    def DFS(self, v):  # Przejście grafu DFS od podanego wierzchołka
+        assert 0 <= v < self.n and self.is_directed() is True
 
         self.getDFS.clear()  # czyścimy, jeżeli wcześniej była używana
-        visited = [False] * self.v  # tablica odwiedzonych wierzchołków
+        visited = [False] * self.n  # tablica odwiedzonych wierzchołków
         self.DFSRecursion(v, visited)  # rozpoczynamy rekurencję
 
         return self.getDFS
@@ -281,15 +228,15 @@ class Graph:
         visited[v] = True
         self.getDFS.append(v)
 
-        for edge in self.adj[v]:
-            if not visited[edge.target]:
-                self.DFSRecursion(edge.target, visited)
+        for target in self.adj[v]:
+            if not visited[target]:
+                self.DFSRecursion(target, visited)
 
-    def BFS(self, v):
-        assert v >= 0 and self.is_vector[v] == 1 and self.is_directed() is True
+    def BFS(self, v):  # przejście grafu BFS od podanego wierzchołka
+        assert 0 <= v < self.n and self.is_directed() is True
         self.getBFS.clear()
 
-        visited = [False] * self.v  # tablica odwiedzonych wierzchołków
+        visited = [False] * self.n  # tablica odwiedzonych wierzchołków
         queue = []
 
         visited[v] = True
@@ -299,30 +246,30 @@ class Graph:
             s = queue.pop(0)
             self.getBFS.append(s)
 
-            for edge in self.adj[s]:
-                if not visited[edge.target]:
-                    visited[edge.target] = True
-                    queue.append(edge.target)
+            for target in self.adj[s]:
+                if not visited[target]:
+                    visited[target] = True
+                    queue.append(target)
 
         return self.getBFS
 
 
 class Tests(unittest.TestCase):
     def setUp(self):
-        self.graph = Graph(10, directed=True)
+        self.graph = Graph(5, directed=True)
         self.graph.add_node(0)
         self.graph.add_node(1)
         self.graph.add_node(2)
         self.graph.add_node(3)
         self.graph.add_node(4)
-        self.graph.add_edge(Edge(0, 1, 3))
+        self.graph.add_edge(Edge(0, 1))
         self.graph.add_edge(Edge(0, 3))
-        self.graph.add_edge(Edge(3, 1, 2))
-        self.graph.add_edge(Edge(1, 4, 1))
-        self.graph.add_edge(Edge(4, 2, 8))
-        self.graph.add_edge(Edge(2, 0, 2))
+        self.graph.add_edge(Edge(3, 1))
+        self.graph.add_edge(Edge(1, 4))
+        self.graph.add_edge(Edge(4, 2))
+        self.graph.add_edge(Edge(2, 0))
 
-        self.undirected_graph = Graph(10, directed=False)
+        self.undirected_graph = Graph(3, directed=False)
         self.undirected_graph.add_node(0)
         self.undirected_graph.add_node(1)
         self.undirected_graph.add_node(2)
@@ -332,34 +279,34 @@ class Tests(unittest.TestCase):
 
     def testCount(self):
         self.assertEqual(self.graph.is_directed(), True)
-        self.assertEqual(self.graph.v, 5)
-        self.assertEqual(self.graph.e, 6)
+        self.assertEqual(self.graph.v(), 5)
+        self.assertEqual(self.graph.e(), 6)
         self.graph.del_node(3)
-        self.assertEqual(self.graph.v, 4)
-        self.assertEqual(self.graph.e, 4)
+        self.assertEqual(self.graph.v(), 5)
+        self.assertEqual(self.graph.e(), 4)
         self.assertEqual(self.graph.has_node(4), True)
-        self.assertEqual(self.graph.has_node(3), False)
-        self.assertEqual(self.graph.has_edge(Edge(4, 2, 8)), True)
-        self.assertEqual(self.graph.has_edge(Edge(3, 1, 2)), False)
-        self.graph.del_edge(Edge(0, 1, 3))
-        self.assertEqual(self.graph.has_edge(Edge(0, 1, 3)), False)
+        self.assertEqual(self.graph.has_node(3), True)
+        self.assertEqual(self.graph.has_edge(Edge(4, 2)), True)
+        self.assertEqual(self.graph.has_edge(Edge(3, 1)), False)
+        self.graph.del_edge(Edge(0, 1))
+        self.assertEqual(self.graph.has_edge(Edge(0, 1)), False)
         self.graph.add_node(3)
         self.graph.add_edge(Edge(0, 3))
-        self.graph.add_edge(Edge(3, 1, 2))
-        self.graph.add_edge(Edge(0, 1, 3))
+        self.graph.add_edge(Edge(3, 1))
+        self.graph.add_edge(Edge(0, 1))
 
     def testIterations(self):
         self.assertEqual(self.graph.iternodes(), [0, 1, 2, 3, 4])
         self.assertEqual(self.graph.iteradjacent(2), [0])
-        self.assertEqual(self.graph.iteroutedges(0), "Edge(0, 1, 3)\nEdge(0, 3)\n")
-        self.assertEqual(self.graph.iterinedges(1), "Edge(0, 1, 3)\nEdge(3, 1, 2)\n")
+        self.assertEqual(self.graph.iteroutedges(0), "Edge(0, 1)\nEdge(0, 3)\n")
+        self.assertEqual(self.graph.iterinedges(1), "Edge(0, 1)\nEdge(3, 1)\n")
         self.assertEqual(self.graph.DFS(3), [3, 1, 4, 2, 0])
         self.assertEqual(self.graph.BFS(0), [0, 1, 3, 4, 2])
 
     def testOther(self):
         self.graph_b = self.graph.transpose()
-        self.assertEqual(self.graph_b.has_edge(Edge(2, 4, 8)), True)
-        self.assertEqual(self.graph_b.has_edge(Edge(1, 3, 2)), True)
+        self.assertEqual(self.graph_b.has_edge(Edge(2, 4)), True)
+        self.assertEqual(self.graph_b.has_edge(Edge(1, 3)), True)
 
         self.graph_c = self.graph.complement()
         self.assertEqual(self.graph_c.has_edge(Edge(0, 0)), True)
@@ -368,22 +315,22 @@ class Tests(unittest.TestCase):
         self.assertEqual(self.graph_c.has_edge(Edge(0, 3)), False)
 
         self.graph_d = self.graph.subgraph([0, 1, 3])
-        self.assertEqual(self.graph_d.iteredges(), "Edge(0, 1, 3)\nEdge(0, 3)\nEdge(3, 1, 2)\n")
+        self.assertEqual(self.graph_d.iteredges(), "Edge(0, 1)\nEdge(0, 3)\nEdge(3, 1)\n")
 
     def testCount2(self):
         self.assertEqual(self.undirected_graph.is_directed(), False)
-        self.assertEqual(self.undirected_graph.v, 3)
-        self.assertEqual(self.undirected_graph.e, 3)
+        self.assertEqual(self.undirected_graph.v(), 3)
+        self.assertEqual(self.undirected_graph.e(), 3)
         self.undirected_graph.del_node(2)
-        self.assertEqual(self.undirected_graph.v, 2)
-        self.assertEqual(self.undirected_graph.e, 1)
-        self.assertEqual(self.undirected_graph.has_node(2), False)
+        self.assertEqual(self.undirected_graph.v(), 3)
+        self.assertEqual(self.undirected_graph.e(), 1)
+        self.assertEqual(self.undirected_graph.has_node(5), False)
         self.assertEqual(self.undirected_graph.has_node(0), True)
         self.assertEqual(self.undirected_graph.has_edge(Edge(0, 1)), True)
         self.assertEqual(self.undirected_graph.has_edge(Edge(1, 2)), False)
         self.undirected_graph.del_edge(Edge(0, 1))
         self.assertEqual(self.undirected_graph.has_edge(Edge(0, 1)), False)
-        self.assertEqual(self.undirected_graph.e, 0)
+        self.assertEqual(self.undirected_graph.e(), 0)
         self.undirected_graph.add_node(2)
         self.undirected_graph.add_edge(Edge(0, 1))
         self.undirected_graph.add_edge(Edge(0, 2))
@@ -399,13 +346,13 @@ class Tests(unittest.TestCase):
         self.undirected_graph_b = self.undirected_graph.transpose()
         self.assertEqual(self.undirected_graph_b.has_edge(Edge(0, 1)), True)
         self.assertEqual(self.undirected_graph_b.has_edge(Edge(2, 1)), True)
-        self.undirected_graph.add_node(4)
+        self.assertRaises(AssertionError, self.undirected_graph.add_node, 4)
 
         self.undirected_graph_c = self.undirected_graph.complement()
-        self.assertEqual(self.undirected_graph_c.has_edge(Edge(0, 4)), True)
-        self.assertEqual(self.undirected_graph_c.has_edge(Edge(4, 1)), True)
-        self.assertEqual(self.undirected_graph_c.has_edge(Edge(4, 2)), True)
+        self.assertEqual(self.undirected_graph_c.has_edge(Edge(0, 4)), False)
+        self.assertEqual(self.undirected_graph_c.has_edge(Edge(0, 1)), False)
         self.assertEqual(self.undirected_graph_c.has_edge(Edge(1, 2)), False)
+        self.assertEqual(self.undirected_graph_c.has_edge(Edge(0, 2)), False)
 
         self.undirected_graph_d = self.undirected_graph.subgraph([0, 1])
         self.assertEqual(self.undirected_graph_d.iteredges(), "Edge(0, 1)\nEdge(1, 0)\n")
